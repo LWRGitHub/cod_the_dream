f43e5400824c8347d83086e8b8ceb51ef3b168cc
LWRGitHub <53380788+LWRGitHub@users.noreply.github.com>
2019-10-28 23:21:17 -0700

    lesen 5 & 4

---
+++ full_name.rb
@@ -0,0 +1,11 @@
+puts "Please enter your first name."
+a = []
+user_input = gets.chomp
+a << user_input
+puts "Please enter your middle name."
+user_input = gets.chomp
+a << user_input
+puts "Please enter your last name."
+user_input = gets.chomp
+a << user_input
+puts  a.inspect
---
+++ sorted_words.rb
@@ -0,0 +1,5 @@
+puts "Please write some words"
+user_input = gets.chomp
+a = user_input.split
+a = a.sort
+puts a
